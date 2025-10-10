63abd88ce8ee2a18ee66aac7393ef9679c095a46
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2025-10-10 10:55:00 +0400

    Border Colors now gets affected by all!

--- sem1Final/src/App.jsx
+++ sem1Final/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
-// Navigation Bar
-function NavigationBar() {
-  const [strokeColor, setStrokeColor] = useState("rgb(0, 140, 255)");
-  
-  const nameColor = {
-    HOME: "rgb(0, 140, 255)",
-    WIKI: "rgba(208, 255, 0, 1)",
-  }
+var themeColor = "rgb(0, 140, 255)"
 
+// Navigation Bar
+function NavigationBar({nameColor, strokeColor, setStrokeColor}) {
   const changeColor = (pageLabel) => {
     setStrokeColor(nameColor[pageLabel]);
+    themeColor = strokeColor
   };
 
   return (
     <nav className="nav-bar" 
       style={{ 
-        borderRight: `4px solid ${strokeColor}`, 
+        borderRight: `4px solid ${themeColor}`, 
         padding: '20px',
         boxSizing: 'border-box',
         transition: 'border-color 0.3s ease-in-out'
@@ -34,7 +30,13 @@ function NavigationBar() {
 function WikiPage({imgUrl, description, stats}) {
   return (
     <div className="Wiki">
-      <div className="LeftPage">
+      <div className="LeftPage"
+      style={{ 
+        border: `4px solid ${themeColor}`, 
+        padding: '20px',
+        boxSizing: 'border-box',
+        transition: 'border-color 0.3s ease-in-out'
+      }}>
         <img 
           src={imgUrl}
           alt="Item Image"
@@ -45,7 +47,13 @@ function WikiPage({imgUrl, description, stats}) {
         </p>
       </div>
 
-      <div className="RightPage">
+      <div className="RightPage"
+      style={{ 
+        border: `4px solid ${themeColor}`, 
+        padding: '20px',
+        boxSizing: 'border-box',
+        transition: 'border-color 0.3s ease-in-out'
+      }}>
         <h1>Stats:</h1>
         <p>
           {stats}
@@ -55,4 +63,19 @@ function WikiPage({imgUrl, description, stats}) {
   );
 }
 
-export {NavigationBar, WikiPage };
+function App({imgUrl, description, stats})  {
+  const [strokeColor, setStrokeColor] = useState("rgb(0, 140, 255)");
+  const nameColor = {
+    HOME: "rgb(0, 140, 255)",
+    WIKI: "rgba(208, 255, 0, 1)",
+  }
+  return (
+    <div>
+        <NavigationBar nameColor={nameColor} strokeColor={strokeColor} setStrokeColor={setStrokeColor} />
+        <WikiPage imgUrl={"https://images.all-free-download.com/images/thumbjpg/camera_test_apple_517290.jpg"} description={"It is an apple bru"} stats={"Its got a lot of nutrients and stuff"}/>
+
+    </div>
+  );
+}
+
+export default App;
--- sem1Final/src/main.jsx
+++ sem1Final/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {NavigationBar, WikiPage } from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavigationBar />
-    <WikiPage imgUrl={"https://images.all-free-download.com/images/thumbjpg/camera_test_apple_517290.jpg"} description={"It is an apple bru"} stats={"Its got a lot of nutrients and stuff"}/>
-
+    <App />
   </StrictMode>,
 )
