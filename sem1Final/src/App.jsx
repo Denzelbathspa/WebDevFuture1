626be9b0fa030222f5bb15b9f860abf8559438a9
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2025-11-02 20:06:43 +0400

    Added background animations. Might not need the exit and just do a blanket cover because it is crowding my css

--- sem1Final/src/App.jsx
+++ sem1Final/src/App.jsx
@@ -1,31 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+// Main
 function App() {
-  const [bgColor, setBgColor] = useState("white")
+  const [isPageChanging, setIsPageChanging] = useState(false);
 
-  function setBGColor(color) {
-    setBgColor(color)
+  function pageChanging(bool_val) {
+    setIsPageChanging(bool_val)
+    
+    // If starting animation, automatically stop it after delay
+    if (bool_val === true) {
+      setTimeout(() => {
+        setIsPageChanging(false);
+      }, 1000);
+    }
   }
 
   return (
     <div className="App">
-      <NavigationBar setBGColor={setBGColor} />
+      <NavigationBar pageChanging={pageChanging} />
       <GameDescription />
       <RobloxLogo />
+      <BGRects pageChanging={isPageChanging} />
     </div>
   )
 }
 
 // Navigation Bar
-function NavigationBar({ setBGColor }) {
+function NavigationBar({ pageChanging }) {
   return (
     <div className="class_NavBar">
       <nav>
-          <button onClick={() => setBGColor("rgb(0, 140, 255)")}>HOME</button>
-          <button onClick={() => setBGColor("rgb(255, 140, 255)")}>LEADERBOARDS</button>
-          <button onClick={() => setBGColor("rgb(0, 140, 0)")}>USERNAME1234</button>
-          <button className='class_AdminButton' onClick={() => setBGColor("rgb(0, 140, 0)")}>ADMIN PANEL</button>
+          <button onClick={() => pageChanging(true)}>HOME</button>
+          <button onClick={() => pageChanging(true)}>LEADERBOARDS</button>
+          <button onClick={() => pageChanging(true)}>USERNAME1234</button>
+          <button className='class_AdminButton' onClick={() => pageChanging(true)}>ADMIN PANEL</button>
       </nav>
     </div>
   )
@@ -63,6 +72,18 @@ function RobloxLogo() {
       <img className='rbx_logo_lower' src='\src\assets\RobloxLogo_Lower.png'></img>
     </div>
   )
+}
+
+// Background Rects
+function BGRects({ pageChanging }) {
+  return (
+    <div className={`simple-grid-bg ${pageChanging ? 'animate-exit' : ''}`}>
+      <div className="rect_1"></div>
+      <div className="rect_2"></div>
+      <div className="rect_3"></div>
+      <div className="rect_4"></div>
+    </div>
+  );
 }
 
 export default App;
